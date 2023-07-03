5062717f7fd857993ca4eb5fe348d2fb96950565
larisoft <larypetero@gmail.com>
2023-07-03 08:16:54 +0100

    created projects

---
+++ movie-catalogue-service/src/main/java/com/larisoftmoviecatalogueservice/movieservice/MovieCatalogueServiceApplication.java
@@ -0,0 +1,23 @@
+package com.larisoftmoviecatalogueservice.movieservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication 
+public class MovieCatalogueServiceApplication {
+
+
+	@LoadBalanced
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogueServiceApplication.class, args);
+	}
+
+}
---
+++ ratings-service/src/main/java/com/larisoftmoviecatalogueservice/ratingservice/MovieRatingServiceApplication.java
@@ -0,0 +1,15 @@
+package com.larisoftmoviecatalogueservice.ratingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication 
+public class MovieRatingServiceApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRatingServiceApplication.class, args);
+	}
+
+}
